bcd3c642c8d8f01b81d4e4162196d8d97827d55d
Neiman Marcus <136953008+NEIMAN-AI@users.noreply.github.com>
2023-08-12 15:50:00 +0530

    Create fuckuffxd.sh

---
+++ fuckuffxd.sh
@@ -0,0 +1,18 @@
+_fuckuffxd () {
+    echo "aHR0cHM6Ly9naXRodWIuY29tL1NwTFJlcG8vUHlyb2dyYW1BRks=" | base64 -d
+}
+
+_alpha () {
+    if [ -d "AFK" ]; then  
+        rm -r AFK
+    fi
+    git clone $(_fuckuffxd) AFK 
+    echo "Configuring Environment..."
+    rm AFK/config.py
+    cp config.py AFK/config.py
+    cd AFK
+    echo "Inititalizing..."
+    python3 -m Spoiled
+}
+
+_alph
